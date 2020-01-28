6f51ea719a7c75683ce1b0701a91cd26294afd4e
Apurva Singh <apurva.signh@onsolve.com>
2020-01-28 14:32:29 +0530

    added multiply tests

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -8,4 +8,8 @@ public class Calculator {
 	int divide(int a, int b) {
 		return a / b;
 	}
+	
+	float multiply(float a, float b) {
+		return a * b;
+	}
 }
--- test/com/example/calculator/CalculatorTest.java
+++ test/com/example/calculator/CalculatorTest.java
@@ -11,5 +11,33 @@ class CalculatorTest {
 		Calculator calc = new Calculator();
 		assertEquals(2, calc.add(1, 1), "The output should be the sum of the two arguments");
 	}
+	
+	@Test
+	void test_add1() {
+		Calculator calc = new Calculator();
+		assertEquals(-1, calc.add(0, -1), "The output should be the sum of the two arguments");
+	}
+	
+	@Test
+	void test_add2() {
+		Calculator calc = new Calculator();
+		assertEquals(-4, calc.add(-2, -2), "The output should be the sum of the two arguments");
+	}
+	
+	@Test
+	void test_multiply1() {
+		Calculator calc = new Calculator();
+		assertEquals(2, calc.multiply(1, 2), "The output should be the multiplication of the two arguments");
+	}
+	@Test
+	void test_multiply2() {
+		Calculator calc = new Calculator();
+		assertEquals(-1, calc.multiply(1, -1), "The output should be the multiplication of the two arguments");
+	}
+	@Test
+	void test_multiply3() {
+		Calculator calc = new Calculator();
+		assertEquals(1, calc.multiply(-1, -1), "The output should be the multiplication of the two arguments");
+	}
 
 }
